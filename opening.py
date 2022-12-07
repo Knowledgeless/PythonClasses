ee88cfc83bf48f37ba15aa694734f79203a900b1
Knowledgeless <nazmulhossain914@gmail.com>
2022-12-07 22:32:41 +0600

    Added Opening Class File

---
+++ opening.py
@@ -0,0 +1,40 @@
+
+
+# print("Hello Public")
+
+# single line commnet out 
+
+'''
+multiple line commnet out
+comment line 1
+comment line 2
+'''
+
+"""
+multiple line commnet out
+comment line 1
+comment line 2
+"""
+
+
+a = 12
+b = 3.14
+c = "A"
+d = "I am a football"
+e = True
+f = "6"
+
+# print(type(a), a)
+# print(type(b), b)
+# print(type(c), c)
+# print(type(d), d)
+# print(type(e), e)
+# print(type(f), f)
+# g = int(f)
+# print(type(g), g)
+# print(a, b, c, d, e)
+
+# print(a+b)
+# print(a+c)
+# print(a*c)
+# print(a/b)
